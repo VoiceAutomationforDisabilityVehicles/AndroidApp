231bbe6f8e1eae0b653b6fb1ab32b7674e82f381
Nick <nicholas.j.liu@gmail.com>
2017-01-11 21:08:21 -0800

    Added Keyword Search

--- app/src/main/java/com/androidapp/voicecontrol/app/MainActivity.java
+++ app/src/main/java/com/androidapp/voicecontrol/app/MainActivity.java
@@ -1,100 +1,110 @@
 package com.androidapp.voicecontrol.app;
-
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-
 import java.util.ArrayList;
-import java.util.Locale;
 
-import android.app.Activity;
-import android.content.ActivityNotFoundException;
-import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.speech.RecognizerIntent;
-import android.view.Menu;
+import android.app.Activity;
+import android.content.Intent;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.ImageButton;
+import android.widget.ListView;
+import android.speech.RecognizerIntent;
 import android.widget.TextView;
-import android.widget.Button;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity implements OnClickListener {
 
+    String[] retractKeys = {"close", "fold", "retract"};
+    String[] openKeys = {"open", "out",};
+
+    public ListView mList;
+    public Button speakButton, status;
     private TextView txtSpeechInput;
-    private Button btnSpeak;
-    private final int REQ_CODE_SPEECH_INPUT = 100;
+
+    public static final int VOICE_RECOGNITION_REQUEST_CODE = 1234;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         txtSpeechInput = (TextView) findViewById(R.id.txtSpeechInput);
-        btnSpeak = (Button) findViewById(R.id.btnSpeak);
 
-        // hide the action bar
-        //getActionBar().hide();
+        status = (Button) findViewById(R.id.status);
 
-        btnSpeak.setOnClickListener(new View.OnClickListener() {
+        speakButton = (Button) findViewById(R.id.btnSpeak);
+        speakButton.setOnClickListener(this);
 
-            @Override
-            public void onClick(View v) {
-                promptSpeechInput();
-            }
-        });
+        voiceinputbuttons();
     }
-    /**
-     * Showing google speech input dialog
-     * */
 
-    private void promptSpeechInput() {
+    public void informationMenu() {
+        startActivity(new Intent("android.intent.action.INFOSCREEN"));
+    }
+
+    public void voiceinputbuttons() {
+        speakButton = (Button) findViewById(R.id.btnSpeak);
+        //mList = (ListView) findViewById(R.id.list);
+    }
+
+    public void startVoiceRecognitionActivity() {
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
                 RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
         intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
-                getString(R.string.speech_prompt));
-
-        try {
-            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
-        } catch (ActivityNotFoundException a) {
-
-            Toast.makeText(getApplicationContext(),
-                    getString(R.string.speech_not_supported),
-                    Toast.LENGTH_SHORT).show();
-        }
+                "Speech recognition demo");
+        startActivityForResult(intent, VOICE_RECOGNITION_REQUEST_CODE);
     }
 
-    /**
-     * Receiving speech input
-     * */
+    public void onClick(View v) {
+        // TODO Auto-generated method stub
+        startVoiceRecognitionActivity();
+    }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        switch (requestCode) {
-            case REQ_CODE_SPEECH_INPUT: {
-                if (resultCode == RESULT_OK && null != data) {
 
-                    ArrayList<String> result = data
-                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-                    txtSpeechInput.setText(result.get(0));
-                }
-                break;
+
+        if (requestCode == VOICE_RECOGNITION_REQUEST_CODE && resultCode == RESULT_OK) {
+            // Fill the list view with the strings the recognizer thought it
+            // could have heard
+            ArrayList<String> results = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+
+            txtSpeechInput.setText(allWords(results));
+            //txtSpeechInput.setText(results.get(0));
+
+            //mList.setAdapter(new ArrayAdapter(this, android.R.layout.simple_list_item_1, matches));
+
+            switch (moveArm(results)){
+                case 1:
+                    status.setBackgroundColor(Color.RED);
+                    break;
+                case 2:
+                    status.setBackgroundColor(Color.GREEN);
             }
 
         }
     }
 
-    /*
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        //getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+    public int moveArm (ArrayList<String> words){
+        for (String word: retractKeys) {
+            if (words.contains(word))
+                return 1;
+        }
+        for (String word: openKeys) {
+            if (words.contains(word))
+                return 2;
+        }
+        return 0;
     }
-    */
 
+    public String allWords (ArrayList<String> words){
+        String temp = "";
+        for (String word: words){
+            temp+= word + ", ";
+        }
+        return temp;
+    }
 }
